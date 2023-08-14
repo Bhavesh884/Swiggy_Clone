e433065ad146a4239527070695b892329d960b25
Bhavesh Bhanusali <bhaveshbhanusalip884@gmail.com>
2023-08-14 12:06:41 +0530

    tweeks 4 done

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -20,16 +20,16 @@ export default Body = () => {
 
   async function getRestaurants() {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.2599333&lng=77.412615&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
       // "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=10584&submitAction=ENTER"
     );
     const json = await data.json();
     console.log(json);
     setrestaurants(
-      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
     console.log(
-      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle.restaurants
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle.restaurants
     );
   }
   return (
--- src/constants.js
+++ src/constants.js
@@ -1,102 +1,602 @@
 export const restaurantlist = [
   {
-    data: {
-      id: 1,
-      name: "Burger Singh",
-      cuisine: ["Burgers, Snacks"],
-      rating: 3.7,
-      image:
-        "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/hmyktxkas3auvpsl728b",
+    info: {
+      id: "43836",
+      name: "McDonald's",
+      cloudinaryImageId: "ee5f8e06b300efc07c9fe3f4df40dfc4",
+      locality: "MG Road",
+      areaName: "Ashok Nagar",
+      costForTwo: "₹400 for two",
+      cuisines: ["Burgers", "Beverages", "Cafe", "Desserts"],
+      avgRating: 4.1,
+      feeDetails: {
+        restaurantId: "43836",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 2500,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 2500,
+      },
+      parentId: "630",
+      avgRatingString: "4.1",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 20,
+        lastMileTravel: 1.4,
+        serviceability: "SERVICEABLE",
+        slaString: "20 mins",
+        lastMileTravelString: "1.4 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-08-15 02:45:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        discountCalloutInfo: {
+          message: "Free Delivery",
+          logoCtx: {
+            logo: "v1655895371/free_delivery_logo_hqipbo.png",
+          },
+        },
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/mcdonalds-mg-road-ashok-nagar-bangalore-43836",
+      type: "WEBLINK",
     },
   },
   {
-    data: {
-      id: 2,
-      name: "Manohar Dairy & Restaurant",
-      cuisine: ["Sweets", "Deserts", "Snacks"],
-      rating: 4.3,
-      image:
-        "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/yhnrql8wxgu8sgqjczos",
+    info: {
+      id: "23847",
+      name: "Domino's Pizza",
+      cloudinaryImageId: "fjqcvqfgqlw6h0atques",
+      locality: "Rest House Road",
+      areaName: "Brigade Road",
+      costForTwo: "₹400 for two",
+      cuisines: ["Pizzas", "Italian", "Pastas", "Desserts"],
+      avgRating: 4.2,
+      feeDetails: {
+        restaurantId: "23847",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 2500,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 2500,
+      },
+      parentId: "2456",
+      avgRatingString: "4.2",
+      totalRatingsString: "5K+",
+      sla: {
+        deliveryTime: 25,
+        serviceability: "SERVICEABLE",
+        slaString: "25 mins",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-08-14 22:59:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹100 OFF",
+        subHeader: "ABOVE ₹199",
+        discountTag: "FLAT DEAL",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/dominos-pizza-rest-house-road-brigade-road-bangalore-23847",
+      type: "WEBLINK",
     },
   },
   {
-    data: {
-      id: 3,
-      name: "Mahadev Bhojnalaya",
-      cuisine: ["North Indian", "South Indian", "Chinese", "Fast Food"],
-      rating: 3.8,
-      image:
-        "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/aipwx1svepzdvsreuszq",
+    info: {
+      id: "65797",
+      name: "Leon's - Burgers & Wings (Leon Grill)",
+      cloudinaryImageId: "r4ufflqojich0r29efvc",
+      locality: "Koramangala",
+      areaName: "Koramangala",
+      costForTwo: "₹300 for two",
+      cuisines: ["American", "Snacks", "Turkish", "Portuguese", "Continental"],
+      avgRating: 4.3,
+      feeDetails: {
+        restaurantId: "65797",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 5800,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 5800,
+      },
+      parentId: "371281",
+      avgRatingString: "4.3",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 31,
+        lastMileTravel: 6.4,
+        serviceability: "SERVICEABLE",
+        slaString: "31 mins",
+        lastMileTravelString: "6.4 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-08-15 04:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        discountCalloutInfo: {
+          message: "Free Delivery",
+          logoCtx: {
+            logo: "v1655895371/free_delivery_logo_hqipbo.png",
+          },
+        },
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/leons-burgers-and-wings-leon-grill-koramangala-bangalore-65797",
+      type: "WEBLINK",
     },
   },
   {
-    data: {
-      id: 4,
-      name: "Rajhans Restaurant",
-      cuisine: ["North Indian", "South Indian", "Chinese", "Fast Food"],
-      rating: 3.7,
-      image:
-        "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/hmyktxkas3auvpsl728b",
+    info: {
+      id: "80802",
+      name: "Third Wave Coffee",
+      cloudinaryImageId: "d96267738c19ec62acb5390e52faba41",
+      locality: "Armane Nagar",
+      areaName: "Sadashiv Nagar",
+      costForTwo: "₹400 for two",
+      cuisines: ["Beverages", "Bakery", "Continental"],
+      avgRating: 4.4,
+      feeDetails: {
+        restaurantId: "80802",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 5000,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 5000,
+      },
+      parentId: "274773",
+      avgRatingString: "4.4",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 32,
+        lastMileTravel: 5.9,
+        serviceability: "SERVICEABLE",
+        slaString: "32 mins",
+        lastMileTravelString: "5.9 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-08-15 01:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        discountCalloutInfo: {
+          message: "Free Delivery",
+          logoCtx: {
+            logo: "v1655895371/free_delivery_logo_hqipbo.png",
+          },
+        },
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/third-wave-coffee-armane-nagar-sadashiv-nagar-bangalore-80802",
+      type: "WEBLINK",
     },
   },
   {
-    data: {
-      id: 5,
-      name: "Rotiwala-Bhopal",
-      cuisine: ["North Indian", "South Indian", "Chinese", "Fast Food"],
-      rating: 3.9,
-      image:
-        "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/brteodziq28wckphkg4f",
+    info: {
+      id: "5938",
+      name: "Burger King",
+      cloudinaryImageId: "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+      locality: "Tasker Town",
+      areaName: "Shivaji Nagar",
+      costForTwo: "₹350 for two",
+      cuisines: ["Burgers", "American"],
+      avgRating: 4.1,
+      feeDetails: {
+        restaurantId: "5938",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 2500,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 2500,
+      },
+      parentId: "166",
+      avgRatingString: "4.1",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 27,
+        lastMileTravel: 2.7,
+        serviceability: "SERVICEABLE",
+        slaString: "27 mins",
+        lastMileTravelString: "2.7 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-08-15 02:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹100 OFF",
+        subHeader: "ABOVE ₹199",
+        discountTag: "FLAT DEAL",
+        discountCalloutInfo: {
+          message: "Free Delivery",
+          logoCtx: {
+            logo: "v1655895371/free_delivery_logo_hqipbo.png",
+          },
+        },
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/burger-king-tasker-town-shivaji-nagar-bangalore-5938",
+      type: "WEBLINK",
     },
   },
   {
-    data: {
-      id: 6,
-      name: "Burger Singh",
-      cuisine: ["Burgers, Snacks"],
-      rating: 3.7,
-      image:
-        "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/hmyktxkas3auvpsl728b",
+    info: {
+      id: "347868",
+      name: "KFC",
+      cloudinaryImageId: "56c9ab92bd79745fd152a30fa2525426",
+      locality: "Cunnigham road",
+      areaName: "Vasanth Nagar",
+      costForTwo: "₹400 for two",
+      cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
+      avgRating: 3.7,
+      feeDetails: {
+        restaurantId: "347868",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 4000,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 4000,
+      },
+      parentId: "547",
+      avgRatingString: "3.7",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 24,
+        lastMileTravel: 3,
+        serviceability: "SERVICEABLE",
+        slaString: "24 mins",
+        lastMileTravelString: "3.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-08-15 03:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "20% OFF",
+        subHeader: "UPTO ₹50",
+        discountCalloutInfo: {
+          message: "Free Delivery",
+          logoCtx: {
+            logo: "v1655895371/free_delivery_logo_hqipbo.png",
+          },
+        },
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/kfc-cunnigham-road-vasanth-nagar-bangalore-347868",
+      type: "WEBLINK",
     },
   },
   {
-    data: {
-      id: 7,
-      name: "Manohar Dairy & Restaurant",
-      cuisine: ["Sweets", "Deserts", "Snacks"],
-      rating: 4.3,
-      image:
-        "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/yhnrql8wxgu8sgqjczos",
+    info: {
+      id: "258",
+      name: "Chinita Real Mexican Food",
+      cloudinaryImageId: "w7vlpanayzxdeshimp7p",
+      locality: "Indiranagar",
+      areaName: "Indiranagar",
+      costForTwo: "₹1000 for two",
+      cuisines: ["Mexican", "Salads", "Desserts", "Beverages"],
+      avgRating: 4.5,
+      feeDetails: {
+        restaurantId: "258",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 6300,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 6300,
+      },
+      parentId: "5064",
+      avgRatingString: "4.5",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 34,
+        lastMileTravel: 6.4,
+        serviceability: "SERVICEABLE",
+        slaString: "34 mins",
+        lastMileTravelString: "6.4 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-08-14 23:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "newg.png",
+            description: "Gourmet",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "Gourmet",
+                  imageId: "newg.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹150 OFF",
+        subHeader: "ABOVE ₹999",
+        discountTag: "FLAT DEAL",
+        discountCalloutInfo: {
+          message: "Free Delivery",
+          logoCtx: {
+            logo: "v1655895371/free_delivery_logo_hqipbo.png",
+          },
+        },
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
     },
-  },
-  {
-    data: {
-      id: 8,
-      name: "Mahadev Bhojnalaya",
-      cuisine: ["North Indian", "South Indian", "Chinese", "Fast Food"],
-      rating: 3.8,
-      image:
-        "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/aipwx1svepzdvsreuszq",
-    },
-  },
-  {
-    data: {
-      id: 9,
-      name: "Rajhans Restaurant",
-      cuisine: ["North Indian", "South Indian", "Chinese", "Fast Food"],
-      rating: 3.7,
-      image:
-        "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/hmyktxkas3auvpsl728b",
-    },
-  },
-  {
-    data: {
-      id: 10,
-      name: "Rotiwala-Bhopal",
-      cuisine: ["North Indian", "South Indian", "Chinese", "Fast Food"],
-      rating: 3.9,
-      image:
-        "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/brteodziq28wckphkg4f",
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/chinita-real-mexican-food-indiranagar-bangalore-258",
+      type: "WEBLINK",
     },
   },
 ];
